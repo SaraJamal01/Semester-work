b3e2b355a5930c9c5a8558637e76887c20f89fdc
SaraJamal01 <83883681+SaraJamal01@users.noreply.github.com>
2021-06-25 20:54:27 +0500

    Strings containing b's only

--- Semester 4/TOA/Some DFAs/4.py
+++ Semester 4/TOA/Some DFAs/4.py
@@ -1,7 +1,7 @@
 import re
 def alphabets(string):
     charRe = re.compile(r'[^b]')     # Python program for all strings containing b's only
-    string = charRe.search(string)   # Regular expression : b+
+    string = charRe.search(string)   # Regular expression: b^+
     return not bool(string)
 
 word = input("Enter string: ")
