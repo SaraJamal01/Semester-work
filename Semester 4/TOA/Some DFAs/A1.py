1544ba166948450835e88591713a8c0fb6ff3ad2
SaraJamal01 <83883681+SaraJamal01@users.noreply.github.com>
2021-06-25 20:56:59 +0500

    Strings containing the substring "ab"

--- Semester 4/TOA/Some DFAs/A1.py
+++ Semester 4/TOA/Some DFAs/A1.py
@@ -2,7 +2,7 @@ from statemachine import StateMachine, State
 
 class FSM(StateMachine):
     s0 = State('s0', initial=True)
-    s1 = State('s1')                   # State machine for all strings containig the substring "ab"
+    s1 = State('s1')                   # State machine for all strings containing the substring "ab"
     s2 = State('s2')
 
     a = s0.to(s1) | s1.to(s1) | s2.to(s2)
